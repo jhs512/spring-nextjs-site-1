1e3547925ceefcfa298e345cd0f899a33176d079
jhs512 <jangka512@gmail.com>
2024-10-01 09:14:40 +0900

    work

---
+++ src/main/java/com/ll/site/SpringNextjsSite1Application.java
@@ -0,0 +1,13 @@
+package com.ll.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNextjsSite1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringNextjsSite1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/site/SpringNextjsSite1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.site;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNextjsSite1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
